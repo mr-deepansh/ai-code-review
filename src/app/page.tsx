38fd443d44bbde2bee9d3ca672f361d1964d73e2
mr-deepansh <deepanshgangwar7037@gmail.com>
2026-06-25 19:47:25 +0530

    feat: add ModeToggle component for theme switching and update Home page to include it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { db } from "@/db";
 import { type InferModel } from "drizzle-orm";
 import { users } from "@/db/schema";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export const dynamic = "force-dynamic";
+type User = typeof users.$inferSelect;
 
 export default async function Home() {
-  let allUsers: InferModel<typeof users>[] = [];
+  let allUsers: User[] = [];
 
   try {
     allUsers = await db.select().from(users);
@@ -15,6 +17,7 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <ModeToggle/>
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1 className="text-xl font-bold">Database Users</h1>
         {allUsers.length > 0 ? (
